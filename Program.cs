29e3565e645f5c7d1d754c73722e2fb4e45a64b6
a.makaruk <askoldmakaruk@gmail.com>
2023-09-27 19:37:00 +0300

    Додав файли

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿System.Console.WriteLine("Введіть число:");
+
+string input;
+int i = 0;
+while (true)
+{
+    input = Console.ReadLine();
+
+    if (int.TryParse(input, out i))
+    {
+        break;
+    }
+    else
+    {
+        System.Console.WriteLine("це не число");
+    }
+}
+
+System.Console.WriteLine(i);
+
+
